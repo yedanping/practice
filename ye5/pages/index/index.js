4a5f08a0a713c93db06bd7546a11579cafd270c0
yedanping <1663029567@qq.com>
2018-01-02 17:11:29 +0800

    text

---
+++ ye5/pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+var initData = 'this is first line\nthis is second line'
+var extraLine = [];
+Page({
+  data: {
+    text: initData
+  },
+  add: function (e) {
+    extraLine.push('other line')
+    this.setData({
+      text: initData + '\n' + extraLine.join('\n')
+    })
+  },
+  remove: function (e) {
+    if (extraLine.length > 0) {
+      extraLine.pop()
+      this.setData({
+        text: initData + '\n' + extraLine.join('\n')
+      })
+    }
+  }
+})
