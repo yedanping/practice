1e8e9a97b309a36dde8733eb9bed16063a74fa8c
yedanping <34825605+yedanping@users.noreply.github.com>
2018-01-04 22:42:05 +0800

    form

---
+++ ye10/pages/index/index.js
@@ -0,0 +1,10 @@
+//index.js
+//获取应用实例
+Page({
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+  }
+})
