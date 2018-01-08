96b3f7364410fd9a4e2fc4244a1dcf000d02e588
yedanping <34825605+yedanping@users.noreply.github.com>
2018-01-08 20:58:16 +0800

    slider

---
+++ ye16/pages/index/index.js
@@ -0,0 +1,11 @@
+//index.js
+//获取应用实例
+var pageData = {}
+for (var i = 1; i < 5; i++) {
+  (function (index) {
+    pageData['slider' + index + 'change'] = function (e) {
+      console.log('slider' + 'index' + '发生 change 事件，携带值为', e.detail.value)
+    }
+  })(i)
+}
+Page(pageData)
