098838c231600c5dab9a2eb127a8408ee4a4418f
yedanping <34825605+yedanping@users.noreply.github.com>
2018-01-09 21:40:03 +0800

    switch

---
+++ ye17/pages/index/index.js
@@ -0,0 +1,10 @@
+//index.js
+//获取应用实例
+Page({
+  switch1Change: function (e) {
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+  switch2Change: function (e) {
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  }
+})
