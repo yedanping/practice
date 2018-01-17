cfc5efc535ce93cd5a92d22e09acd23ea0dba86d
yedanping <34825605+yedanping@users.noreply.github.com>
2018-01-17 22:33:06 +0800

    liver-player

---
+++ ye24/pages/index/index.js
@@ -0,0 +1,10 @@
+//index.js
+//获取应用实例
+Page({
+  statechange(e) {
+    console.log('live-player code:', e.detail.code)
+  },
+  error(e) {
+    console.error('live-player error:', e.detail.errMsg)
+  }
+})
