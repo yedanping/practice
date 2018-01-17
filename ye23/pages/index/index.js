1f4d823d90a22d19964fd85abf757f71ff9b6bd3
yedanping <34825605+yedanping@users.noreply.github.com>
2018-01-17 22:28:07 +0800

    camera

---
+++ ye23/pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+// camera.js
+Page({
+  takePhoto() {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  }
+})
